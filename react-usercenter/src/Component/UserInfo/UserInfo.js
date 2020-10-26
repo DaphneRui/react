5b02f5d102d5c681ba925b2c39069f970564e6f5
ruixiang <1900956090@qq.com>
2020-10-26 13:56:29 +0800

    react

--- react-usercenter/src/Component/UserInfo/UserInfo.js
+++ react-usercenter/src/Component/UserInfo/UserInfo.js
@@ -6,13 +6,6 @@ import { userList } from '../../Redux/Action/userInfo';
 export default function UserInfo () {
    const dispatch = useDispatch();
    const user = useSelector(state=>state.userInfo.list);
-   let [ isGirl,setIsGirl ] = useState(true);
-   useEffect(() => {
-      dispatch(userList());
-      if(user.gender === 1){
-         setIsGirl(false);
-      }
-   }, []);
 
    return (
       <div className="information">
@@ -21,7 +14,7 @@ export default function UserInfo () {
             <span>姓名 :<p>{user.username}</p></span>
             <span>学校 :<p>{user.school}</p></span>
             <span>昵称 :<p>{user.nickname}</p></span>
-            <span>性别 :{ isGirl ? <p>女</p> : <p>男</p>}</span>
+            <span>性别 :{ user.gender === 1 ? <p>女</p> : <p>男</p>}</span>
             <span>密码 :<p>{ user.password }</p></span>
          </div>
       </div>
--- react-usercenter/src/Component/Usercenter/Usercenter.js
+++ react-usercenter/src/Component/Usercenter/Usercenter.js
@@ -3,30 +3,37 @@ import './usercenter.css';
 import UserInfo from '../UserInfo/UserInfo';
 import Record from '../Record/Record';
 export default function User () {
-   let [ isUser,setIsUser ] = useState(true);
-   let [ isRecord,setIsRecord ] = useState(false);
 
+   let [ isTab,setIsTab ] = useState('user');
+   function handleDemo (){
+      switch (isTab) {
+      case 'user':
+         return (<UserInfo/>);
+      case 'record':
+         return(<Record/>);
+      default:
+         return null;
+      }
+   }
    return (
       <div className="user">
          <div className="tabs">
             <div className="tab" onClick={ ()=>{
-               setIsUser(true);
-               setIsRecord(false);
+               setIsTab('user');
             } }>
                <h1>个人信息</h1>
-               {isUser ? <div className="line"></div> : null}
+               {isTab === 'user' ? <div className="line"></div> : null}
             </div>
             <div className="tab" onClick={ ()=>{
-               setIsRecord(true);
-               setIsUser(false);
+               setIsTab('record');
             } }>
                <h1>兑换记录</h1>
-               {isRecord ? <div className="line"></div> : null}
+               {isTab === 'record' ? <div className="line"></div> : null}
             </div>
          </div>
 
          <div className="card">
-            {isUser ? <UserInfo/> : <Record/>}
+            {handleDemo()}
          </div>
       </div>
    );
